2361df2656b883c6aa627a1824657d21431b9caa
Gonçalo Santos <mankin.pt@gmail.com>
2017-10-08 19:57:02 +0100

    Setup project
    + Setup initial libgdx setup (android & desktop)

---
+++ desktop/src/com/homesweathome/dandelionproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.homesweathome.dandelionproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.homesweathome.dandelionproject.DandelionProjectGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DandelionProjectGame(), config);
+	}
+}
